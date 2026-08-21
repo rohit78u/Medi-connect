649a6cadc4cd1669699164db51fcc707a920f186
Rohit Pawar <155699779+rohit78u@users.noreply.github.com>
2026-08-21 22:28:46 +0530

    fix: remove unused dependency imports

--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -1,6 +1,6 @@
 import uuid
 from typing import Callable, List
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 
